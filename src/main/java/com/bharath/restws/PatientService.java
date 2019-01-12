ad13a16f566d25caea51e0e90c619c80729c7dbf
kongaman <christian.kranz@gmail.com>
2019-01-12 16:54:04 +0100

    classes created

---
+++ src/main/java/com/bharath/restws/PatientService.java
@@ -0,0 +1,5 @@
+package com.bharath.restws;
+
+public interface PatientService {
+
+}
---
+++ src/main/java/com/bharath/restws/PatientServiceImpl.java
@@ -0,0 +1,5 @@
+package com.bharath.restws;
+
+public class PatientServiceImpl implements PatientService {
+
+}
