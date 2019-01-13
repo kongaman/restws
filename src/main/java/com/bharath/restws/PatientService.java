42d2011868f5c040b2e64ab8a2327677b50e21b9
kongaman <christian.kranz@gmail.com>
2019-01-13 14:11:39 +0100

    added method to create a patient and tested with postman -> works

--- src/main/java/com/bharath/restws/PatientService.java
+++ src/main/java/com/bharath/restws/PatientService.java
@@ -3,8 +3,10 @@ package com.bharath.restws;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 import com.bharath.restws.model.Patient;
 
@@ -18,5 +20,9 @@ public interface PatientService {
 	@Path("/patients/{id}") // {id} = Path-Variable will be bound to "(Long id)" on next line because of the @PathParam annotation
 	@GET
 	Patient getPatient(@PathParam(value = "id") Long id);
+	
+	@Path("/patients")
+	@POST
+	Response createPatient(Patient patient);
 
 }
--- src/main/java/com/bharath/restws/PatientServiceImpl.java
+++ src/main/java/com/bharath/restws/PatientServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.core.Response;
+
 import org.springframework.stereotype.Service;
 
 import com.bharath.restws.model.Patient;
@@ -37,6 +39,13 @@ public class PatientServiceImpl implements PatientService {
 	@Override
 	public Patient getPatient(Long id) {
 		return patients.get(id);
+	}
+
+	@Override
+	public Response createPatient(Patient patient) {
+		patient.setId(++currentId);
+		patients.put(patient.getId(), patient);
+		return Response.ok(patient).build();
 	}
 
 }
