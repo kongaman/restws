d35409b66f1ef745674966bf1bd255496158bdd8
kongaman <christian.kranz@gmail.com>
2019-01-16 10:53:04 +0100

    configured patientservice to produce and consuem xml AND JSON

--- src/main/java/com/bharath/restws/PatientService.java
+++ src/main/java/com/bharath/restws/PatientService.java
@@ -2,16 +2,20 @@ package com.bharath.restws;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import com.bharath.restws.model.Patient;
 
+@Consumes("application/xml,application/json") //can be used on the methods too
+@Produces("application/xml,application/json") //maybe to let a method create only JSON or only XML (rarely used this way)
 @Path("/patientservice")
 public interface PatientService {
 	
